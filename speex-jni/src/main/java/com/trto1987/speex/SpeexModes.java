668a67e71c8daec25d8cdc3233cf6cc66fb7875d
trto1987 <trto1987@outlook.com>
2019-10-16 16:54:05 +0800

    fix SpeexModes enum bug

--- speex-jni/src/main/java/com/trto1987/speex/SpeexModes.java
+++ speex-jni/src/main/java/com/trto1987/speex/SpeexModes.java
@@ -2,12 +2,16 @@ package com.trto1987.speex;
 
 public enum SpeexModes {
     /** narrowband mode */
-    SPEEX_MODE_NB,
+    SPEEX_MODE_NB(0),
     /** wideband mode */
-    SPEEX_MODE_WB,
+    SPEEX_MODE_WB(1),
     /** ultra-wideband mode */
-    SPEEX_MODE_UWB,
+    SPEEX_MODE_UWB(2),
     ;
+
+    SpeexModes(int mode) {
+        this.mode = mode;
+    }
 
     public int getMode() {
         return mode;
